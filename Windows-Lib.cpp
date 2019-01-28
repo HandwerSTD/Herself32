02969da38dfd2a5a22e680440c0a64efa923a94b
MoveToEx <movetoex@outlook.com>
2019-01-28 08:38:28 +0800

    Add Windows-Lib

---
+++ Windows-Lib.cpp
@@ -0,0 +1,10 @@
+#include <windows.h>
+
+int WINAPI WinMain(
+	HINSTANCE hInst,
+	HINSTANCE hPrevInst,
+	LPTSTR lpCmdLine,
+	int nCmdShow) {
+	MessageBox(nullptr, TEXT("Herself32 %%%"), TEXT("%%%"), MB_OK);
+	return 0;
+}
